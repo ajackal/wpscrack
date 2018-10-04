ec29f5cb7f9a7cbaeee5e4a5b90b29bfc8446c1e
chris work hp <ajacjal244@gmail.com>
2018-10-04 17:54:10 -0400

    modified ByteFields to get proper packet output.

--- wpscrack/Dot11HTCapabilities.py
+++ wpscrack/Dot11HTCapabilities.py
@@ -18,20 +18,14 @@ class Dot11EltHighThroughputCapabilities(Packet):
 
     fields_desc = [ByteField("ID", 45),
                    ByteField("len", 26),
-                   ByteField("HT Capabilities Info 1", 0x20),
-                   ByteField("HT Capabilities Info 2", 0x00),
-                   ByteField("A-MPDU Parameters", 0x1a)]
+                   XByteField("HT Capabilities Info 1", 0x20),
+                   XByteField("HT Capabilities Info 2", 0x00),
+                   XByteField("A-MPDU Parameters", 0x1a)]
 
     for rate in rx_supported_mcs:
-        fields_desc.append(ByteField(rate[0], rate[1]))
+        fields_desc.append(XByteField(rate[0], rate[1]))
 
-    fields_desc.append(ByteField("Highest Supported Data Rate 0", 0x00))
-    fields_desc.append(ByteField("Highest Supported Data Rate 1", 0x0))
-
-    fields_desc.append(ByteField("HT Extended Capabilities 0", 0x00))
-    fields_desc.append(ByteField("HT Extended Capabilities 1", 0x0))
-
-    for cap in range(0, 3):
-        fields_desc.append(ByteField("Transmit Beam Forming{0}".format(str(cap)), 0x00))
-
-    fields_desc.append(ByteField("Antenna Selection Capabilities", 0x00))
+    fields_desc.append(XShortField("Highest Supported Data Rate", 0x000))
+    fields_desc.append(XShortField("HT Extended Capabilities", 0x000))
+    fields_desc.append(XLongField("Transmit Beam Forming", 0x00000000))
+    fields_desc.append(XByteField("Antenna Selection Capabilities", 0x00))
