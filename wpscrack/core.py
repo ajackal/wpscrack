010beaa7c6743a90655dfbe9f5778731144f5673
chris work hp <ajacjal244@gmail.com>
2018-10-04 18:00:22 -0400

    added Dot11EltHighThroughputCapabilties back into core

--- wpscrack/core.py
+++ wpscrack/core.py
@@ -41,6 +41,34 @@ class Dot11EltExtendedSupportedRates(Packet):
         fields_desc.append(ByteField("extended_supported_rate{0}".format(index), rate))
         index += 1
 
+class Dot11EltHighThroughputCapabilities(Packet):
+    """ Definition of the High Throughput Capabilities for 802.11n"""
+    name = "802.11n High Throughput Capabilities Informational Element"
+    rx_supported_mcs = [("RX Bitmask Bits 0-7", 0x3f),
+                        ("RX Bitmask Bits 8-15", 0x00),
+                        ("RX Bitmask Bits 16-23", 0x00),
+                        ("RX Bitmask Bits 24-31", 0x00),
+                        ("RX Bitmask Bits 32", 0x0),
+                        ("RX Bitmask Bits 33-38", 0x00),
+                        ("RX Bitmask Bits 39-52a", 0x00),
+                        ("RX Bitmask Bits 39-52b", 0x00),
+                        ("RX Bitmask Bits 53-76a", 0x00),
+                        ("RX Bitmask Bits 53-76b", 0x00),
+                        ("RX Bitmask Bits 53-76c", 0x00)]
+
+    fields_desc = [ByteField("ID", 45),
+                   ByteField("len", 26),
+                   XShortField("HT Capabilities Info", 0x2000),
+                   XByteField("A-MPDU Parameters", 0x1a)]
+
+    for rate in rx_supported_mcs:
+        fields_desc.append(XByteField(rate[0], rate[1]))
+
+    fields_desc.append(XShortField("Highest Supported Data Rate", 0x000))
+    fields_desc.append(XShortField("HT Extended Capabilities", 0x000))
+    fields_desc.append(XLongField("Transmit Beam Forming", 0x00000000))
+    fields_desc.append(XByteField("Antenna Selection Capabilities", 0x00))
+
 
 class WPSCrack:
     verbose = None
@@ -176,10 +204,7 @@ class WPSCrack:
             / Dot11Elt(info=self.ssid, ID=0, len=len(self.ssid)) \
             / Dot11EltSupportedRates() \
             / Dot11EltExtendedSupportedRates() \
-            / b"\x2d\x1a\x20\x00\x1a\x3f\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x10\x00\x00\x00\x00\x00\x00\x00" \
-              b"\x00\x00\x00\x00\x00\xdd\x1e\x00\x90\x4c\x33\x20\x00\x1a\xff\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
-              b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xdd\x07\x00\x50\xf2\x02\x00\x01\x00"
-        # / Dot11EltHighThroughputCapabilities()
+            / Dot11EltHighThroughputCapabilities()
 
         # / Dot11Elt(info='\x02\x04\x0b\x16\x0c\x12\x18$', ID=1, len=8) \
         # / Dot11Elt(info='0H`l', ID=50, len=4) \
