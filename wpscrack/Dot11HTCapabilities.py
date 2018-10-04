c28fe2995d83c4fae6cd6e220aac914aa3e0ee02
chris work hp <ajacjal244@gmail.com>
2018-10-04 08:23:27 -0400

    scapy misorders HT Caps, using raw binary stream for now

--- wpscrack/Dot11HTCapabilities.py
+++ wpscrack/Dot11HTCapabilities.py
@@ -15,6 +15,7 @@ class Dot11EltHighThroughputCapabilities(Packet):
                         "rx_bit_max53-76a": 0x00,
                         "rx_bit_max53-76b": 0x00,
                         "rx_bit_max53-76c": 0x00}
+
     fields_desc = [ByteField("ID", 45),
                    ByteField("len", 26),
                    ByteField("ht_capabilities_info1", 0x20),
@@ -24,7 +25,13 @@ class Dot11EltHighThroughputCapabilities(Packet):
     for rate in rx_supported_mcs:
         fields_desc.append(ByteField(rate, rx_supported_mcs[rate]))
 
+    fields_desc.append(ByteField("highest_supported_data_rate0", 0x00))
+    fields_desc.append(ByteField("highest_supported_data_rate1", 0x0))
 
-                   # ByteField("ht_extended_capabilities", 0x00),
-                   # ByteField("transmit_beam_forming", 0x00),
-                   # ByteField("antenna_selection_capabilities", 0x00)
+    fields_desc.append(ByteField("ht_extended_capabilities0", 0x00))
+    fields_desc.append(ByteField("ht_extended_capabilities1", 0x0))
+
+    for cap in range(0, 3):
+        fields_desc.append(ByteField("transmit_beam_forming{0}".format(str(cap)), 0x00))
+
+    fields_desc.append(ByteField("antenna_selection_cap", 0x00))
